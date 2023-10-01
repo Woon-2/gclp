51bcd02395c0e9ce822042f26d27cf715c76ca25
Woon2 <73771162+Woon-2@users.noreply.github.com>
2023-10-01 11:43:22 +0900

    [test](main): add tests ParseMutipleArgs, OmitOptional, IgnoresSpaceInQuoted

--- test/gclp_test.cpp
+++ test/gclp_test.cpp
@@ -26,6 +26,7 @@ THE SOFTWARE.
 #include "cl_parser.hpp"
 #include <string>
 #include <string_view>
+#include <tuple>
 
 using namespace std::literals;
 
@@ -33,7 +34,7 @@ TEST(HelloTestSuite, HelloTest) {
     ASSERT_EQ(1, 1);
 }
 
-TEST(BasicFeatureTest, CanParseSingleOptional) {
+TEST(BasicParsingTest, ParseSingleOptional) {
     auto parser = clp::parser(
         "identifier"sv,
         clp::optional<int>(
@@ -47,7 +48,7 @@ TEST(BasicFeatureTest, CanParseSingleOptional) {
     EXPECT_EQ(aa, 4);
 }
 
-TEST(BasicFeatureTest, CanParseSingleRequired) {
+TEST(BasicParsingTest, ParseSingleRequired) {
     auto parser = clp::parser(
         "identifier"sv,
         clp::required<int>(
@@ -59,4 +60,94 @@ TEST(BasicFeatureTest, CanParseSingleRequired) {
     EXPECT_EQ(a, 3);
     auto [aa] = parser.parse("identifier --aa 4"sv);
     EXPECT_EQ(aa, 4);
+}
+
+class ParsingTest : public ::testing::Test {
+protected:
+    ParsingTest()
+        : a(
+            {'a', 'A', 'i', 'I'},
+            {"aa", "AA", "int", "integer", "Integer"},
+            "an optional integer parameter"
+        ),
+        b(
+            {'b', 'B', 'd', 'D'},
+            {"bb", "BB", "double", "real", "Double", "Real"},
+            "an optional real parameter"
+        ),
+        c(
+            {'c', 'C'},
+            {"cc", "CC", "char", "character", "Character"},
+            "a required character parameter"
+        ),
+        d(
+            {'d', 'D', 's', 'S'},
+            {"dd", "DD", "string", "String"},
+            "a required string parameter"
+        ),
+        e(
+            {'e', 'E'},
+            {"ee", "EE"},
+            "an optional string parameter"
+        ),
+        f(
+            {'f', 'F'},
+            {"ff", "FF", "float", "Float"},
+            "an optional float parameter"
+        ),
+        g(
+            {'g', 'G', 'u', 'U'},
+            {"gg", "GG", "unsigned_short", "UnsignedShort", "ushort"},
+            "a required unsigned short parameter"
+        ),
+        parser("TestCLI"sv, a, b, c, d, e, f, g) {}
+
+private:
+    clp::optional<int> a;
+    clp::optional<double> b;
+    clp::required<char> c;
+    clp::required<std::string> d;
+    clp::optional<std::string> e;
+    clp::optional<float> f;
+    clp::required<unsigned short> g;
+
+protected:
+    clp::parser<
+        decltype(a), decltype(b), decltype(c),
+        decltype(d), decltype(e), decltype(f),
+        decltype(g)
+    > parser;
+};
+
+TEST_F(ParsingTest, ParseMultipleArgs) {
+    auto cmd = "TestCLI -a 1 -b 3.14 -c c -d Hello -e World! -f 1.6 g 1"sv;
+    auto res = parser.parse(cmd);
+
+    ASSERT_FALSE(parser.error()) << parser.error_message();
+
+    EXPECT_EQ(res, std::make_tuple(1, 3.14, 'c', "Hello"s, "World!"s, 1.6, 1));
+}
+
+TEST_F(ParsingTest, OmitOptional) {
+    auto cmd = "TestCLI -c c -d Hello -g 1"sv;
+    auto [ra, rb, rc, rd, re, rf, rg] = parser.parse(cmd);
+
+    ASSERT_FALSE(parser.error()) << parser.error_message();
+
+    EXPECT_EQ(rc, 'c');
+    EXPECT_EQ(rd, "Hello");
+    EXPECT_EQ(rg, 1u);
+}
+
+TEST_F(ParsingTest, IgnoresSpaceInQuoted) {
+    auto cmd = "TestCLI -a 1 -b 3.14 -c c -d \"He llo\" -e \"Wo rld ! \" -f 1.6 g 1"sv;
+    auto [ra, rb, rc, rd, re, rf, rg] = parser.parse(cmd);
+
+    ASSERT_FALSE(parser.error() && parser.error()
+        != clp::error_code::unparsed_argument
+    ) << parser.error_message();
+
+    EXPECT_FALSE(parser.error() && parser.error()
+        == clp::error_code::unparsed_argument
+    ) << "remainders are from not ignoring space within quote.";
 }
